9668e1a9287a9c2aa40efacc746315b24f10adc8
module1 <praveenkayyala@gmail.com>
2018-01-22 09:35:23 +0000

    SampleTest added

---
+++ src/test/java/com/test/SampleTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Sudhakar on 22/01/2018.
+ */
+public class SampleTest {
+    @Test
+    public void test(){
+        System.out.println("Sample testing ..");
+    }
+}
