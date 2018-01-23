9600cad246b9e4c3f7d2cbfa4a1797f980bd9e66
Sudhakar Kayyala <sudhakarkayyala@gmail.com>
2018-01-23 15:29:36 +0000

    sampletest updated

--- src/test/java/com/test/SampleTest.java
+++ src/test/java/com/test/SampleTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 public class SampleTest {
     @Test
     public void test(){
-        System.out.println("Sample testing ..");
+        System.out.println("Sample testing .ysgfuhskhkjggh" +
+                ".");
     }
 }
